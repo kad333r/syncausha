369f2f546dae5c905f760ad668b30fdfaf58c41e
Kader <elkalidi@silicon.ma>
2026-09-23 09:28:16 +0000

    chore: squelette du projet SyncAusha

---
+++ syncausha/__init__.py
@@ -0,0 +1,3 @@
+"""SyncAusha : publie automatiquement sur Ausha les podcasts d'un dossier."""
+
+__version__ = "1.0.0"
---
+++ tests/test_package.py
@@ -0,0 +1,5 @@
+import syncausha
+
+
+def test_version():
+    assert syncausha.__version__ == "1.0.0"
